602890f988ce4e8a9cadab99e7b697fd28828ece
Sean Dominguez <sean@drivelous.com>
2016-08-06 18:23:04 -0400

    starting testing redux

---
+++ src/components/course/CourseForm.Enzyme.test.js
@@ -0,0 +1,32 @@
+import expect from 'expect';
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import CourseForm from './CourseForm';
+
+function setUp(saving) {
+	const props = {
+		course: {}, saving: saving, errors: {},
+		onSave: () => {},
+		onChange: () => {}
+	};
+
+	return shallow(<CourseForm {...props} />);
+}
+
+describe('CourseForm via Enzyme', () => {
+	it('renders ofrm and h1', () => {
+		const wrapper = setUp(false);
+		expect(wrapper.find('form').length).toBe(1);
+		expect(wrapper.find('h1').text()).toEqual('Manage Course');
+	});
+
+	it('save button is labeled "Save" when not saving', () => {
+		const wrapper = setUp(false);
+		expect(wrapper.find('input').props().value).toBe('Save');
+	});
+
+	it('save button is labeled "Saving..." when saving', () => {
+		const wrapper = setUp(true);
+		expect(wrapper.find('input').props().value).toBe('Saving...');
+	});
+});
